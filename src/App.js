fcf9b459b123ca358b8e5aef476de520fc837f98
sashi <114sashi@gmail.com>
2023-04-05 02:28:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section>
+        <header>
+          <ul>
+            <li><a>Home</a></li>
+            <li><a>About</a></li>
+            <li><a>Contact</a></li>
+            <li><a>FAQ</a></li>
+          </ul>
+        </header>
+      </section>
+      <section>
+        <div>
+          <h3>Hello Nadge</h3>
+        </div>
+      </section>
+      <section>
+        <footer>
+          <p>copy@rights@nadge.err</p>
+        </footer>
+      </section>
+    </div>
+  );
+}
+
+export default App;
